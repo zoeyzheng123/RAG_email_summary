4f3fab7fce06edcd648eb86d39adc798545d6a2c
Zoey Zheng <43158126+zoeyzheng123@users.noreply.github.com>
2025-01-15 15:43:15 -0500

    remove redundant files

--- src/config.py
+++
@@ -1,5 +0,0 @@
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-
-output_dir = "emails_txt"
-local_llm = "llama3.2"
-cred_path = None
--- src/setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name='predictor',
-    version='0.0.1',
-    description='Predictor pipeline',
-    install_requires=['dynaconf==3.1.12', 'sentry-sdk==1.31.0', "backoff==2.2.1"],
-    packages=setuptools.find_packages(exclude=['*.tests', 'tests']),
-)
